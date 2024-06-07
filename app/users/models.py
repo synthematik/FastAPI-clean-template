bf2d1e7366fa323a5fcb1ddb6343d5b28be88e98
Garik <garfild_2003@mail.ru>
2024-06-07 17:27:39 +0300

    Updated User model

--- app/users/models.py
+++ app/users/models.py
@@ -1,10 +1,12 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
 from app.database import Base
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column("id", Integer, primary_key=True, index=True)
-    email = Column("email", String, nullable=False)
-    hashed_password = Column("password", String)
+    id: Mapped[int] = mapped_column("id", primary_key=True, index=True)
+    email: Mapped[str] = mapped_column("email", unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column("password", nullable=False)
